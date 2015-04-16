5721fcb31e38a25baf06dc84c7e2578e0976b1c7
Honshi <honshio@t20group.com.au>
2015-04-16 13:54:03 +1000

    Test v5

--- js/sensors-text.js
+++ js/sensors-text.js
@@ -84,8 +84,11 @@ function sensor_poll() {
 		$('#sonartext').prepend(d+'<br/>');
 		// sensed someone within zone, send to game page
 		if ( (d>=20 && d<=60) && firstRun==false ) { 
-			window.location.href = "index-sonar-next.html";
+			//window.location.href = "index-sonar-next.html";
 			$('#sonartext').prepend('WITHIN range: '+d+'<br/>');
+			$('#touchme').animate(
+				{opacity: 1}, 200
+			);
 		}
 		
 
